8ab9d8a44465d9b48d370269ec74b27ac811d205
yutian.liu <939029835@qq.com>
2021-06-02 10:19:24 +0800

    新增EXUIImage扩展支持

--- Source_extern/Objc.hx
+++ Source_extern/Objc.hx
@@ -1,3 +1,5 @@
+import haxe.macro.Expr.ExprDef;
+
 macro function UIAlertActionCall(call:Dynamic):Dynamic {
 	return macro {
 		untyped __cpp__("^(UIAlertAction * _Nonnull action){
@@ -25,5 +27,22 @@ macro function CGRectMake(x:Dynamic, y:Dynamic, width:Dynamic, height:Dynamic):D
 macro function CGPointMake(x:Dynamic, y:Dynamic):Dynamic {
 	return macro {
 		untyped __cpp__("CGPointMake({0}, {1})", ${x}, ${y});
+	}
+}
+
+/**
+ * 颜色转换，透明度永远为1
+ * @param color 传递0x000000颜色值
+ * @return Dynamic
+ */
+macro function UIColorRGB(color:Dynamic):Dynamic {
+	var expr:ExprDef = color.expr;
+	expr = expr.getParameters()[0];
+	var selfcolor:Int = Std.parseInt(expr.getParameters()[0]);
+	var r = (selfcolor >> 16) & 0xFF;
+	var g = (selfcolor >> 8) & 0xFF;
+	var b = selfcolor & 0xFF;
+	return macro {
+		ios.uikit.UIColor.colorWithRedGreenBlueAlpha($v{r} / 255, $v{g} / 255, $v{b} / 255, 1);
 	}
 }
---
+++ Source_extern/ext/EXUIImage.hx
@@ -0,0 +1,22 @@
+package ext;
+
+import ios.uikit.UIColor;
+import Objc.UIColorRGB;
+import Objc.CGRectMake;
+import ios.uikit.UIImage;
+import ios.objc.CGRect;
+
+class EXUIImage {
+	public static function imageWithColor(color:UIColor):UIImage {
+		var rect:CGRect = CGRectMake(0, 0, 1, 1);
+		untyped __cpp__("UIGraphicsBeginImageContext({0}.size)", rect);
+		untyped __cpp__("CGContextRef context = UIGraphicsGetCurrentContext();CGContextSetFillColorWithColor(context, [{0} CGColor]);CGContextFillRect(context, {1});",
+			color,
+			rect);
+		untyped __cpp__("UIGraphicsEndImageContext()");
+		untyped __cpp__("UIGraphicsEndImageContext()");
+		var image:UIImage = untyped __cpp__("UIGraphicsGetImageFromCurrentImageContext()");
+		untyped __cpp__("UIGraphicsEndImageContext()");
+		return image;
+	}
+}
