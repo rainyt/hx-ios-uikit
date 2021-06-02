17bafbbffb108855064bfa5f9e9d084c02f983ad
yutian.liu <939029835@qq.com>
2021-06-02 21:47:45 +0800

    新增dispatch_async

--- Source_extern/Objc.hx
+++ Source_extern/Objc.hx
@@ -20,6 +20,14 @@ macro function UIAlertActionCall(call:Dynamic):Dynamic {
 	};
 }
 
+macro function dispatch_async(call:Dynamic):Dynamic {
+	return macro {
+		untyped __cpp__('dispatch_async(dispatch_get_main_queue(), ^(){  
+			{0}();
+		});', ${call});
+	}
+}
+
 macro function NSData_NSURLResponse_NSError(call:Dynamic):Dynamic {
 	return macro {
 		untyped __cpp__("^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error){
